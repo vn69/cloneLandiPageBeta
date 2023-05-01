bbce713c1c8fb9c6bb148f38b16ef289ab48668f
Doan_na <doanna@mpos.vn>
2023-05-01 17:43:55 +0700

    Adding dist

---
+++ assets/ShowDragItem-bb517afa.js
@@ -0,0 +1,224 @@
+import{bu as i,az as l,bv as s,at as o,p as d,ad as r,af as c}from"./index-6012b17a.js";import{_ as p}from"./_plugin-vue_export-helper-c27b6911.js";const g={listItem:[{id:"1682932595173",content:`<div class="render-item" data-draggable="true">
+<div class="element-group-main">
+<div class="min-padding">
+<div class="layout-render cvo-text-color no-padding flex-row flex-nowrap">
+<div class="render-item" data-draggable="true">
+<div class="element-main element-type-text-single component-render" data-v-5cee62ab="">
+<div class="ql-editor" data-v-5cee62ab="">
+<p><strong class="ql-font-Barlow ql-size-18px">SUMMARY</strong></p>
+<ul>
+<li>Highly skilled frontend developer with 2 years of experience working with Vue.js. Proficient in HTML, CSS, and JavaScript. Strong understanding of responsive design, user experience, and performance optimization. Passionate about building modern, engaging web applications.</li>
+<li>I am a self-taught developer. I am seeking opportunities to develop myself further and contribute to the company.</li>
+<li>My personal goals are to enhance my frontend skills and become a senior full-stack developer within the next 2 years.</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>`,type:"text",boxItem:{x:60,y:117,h:134,w:1080,active:!1},active:!1},{id:"1682932663785",content:`<div class="render-item" data-draggable="true">
+<div class="element-main element-type-text-single component-render" data-v-5cee62ab="">
+<div class="ql-editor" data-v-5cee62ab="">
+<p><span style="font-size: 18pt;"><strong class="ql-font-Barlow ql-size-30px">Nguyễn Anh Đo&agrave;n</strong></span></p>
+<p>Frontend Website Developer</p>
+</div>
+</div>
+</div>`,type:"text",boxItem:{x:60,y:0,h:97,w:220,active:!1},active:!1},{id:"1682932732869",content:`<div class="render-item" data-draggable="true">
+<div class="block-child-main">
+<div class="min-padding">
+<div class="layout-render no-padding">
+<div class="render-item" data-draggable="true">
+<div class="element-group-main">
+<div class="min-padding">
+<div class="layout-render no-padding flex-row flex-nowrap">
+<div class="render-item" data-draggable="true">
+<div class="element-main element-type-text-single component-render" data-v-5cee62ab="">
+<div class="ql-editor" data-v-5cee62ab="">
+<ul>
+<li><span class="ql-font-Barlow ql-size-13px">Phone: 0 888 115 745</span></li>
+<li><span class="ql-font-Barlow ql-size-13px">Email: anhdoannguyen95@gmail.com</span></li>
+<li><span class="ql-font-Barlow ql-size-13px">Adress: Đống Đa, H&agrave; Nội</span></li>
+<li><span class="ql-font-Barlow ql-size-13px">Project: <strong class="ql-font-Barlow ql-size-13px">https://github.com/vn69 </strong></span></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="render-item" data-draggable="true">
+<div class="block-child-main">
+<div class="min-padding">
+<div class="layout-render no-padding">
+<div class="render-item" data-draggable="true">
+<div class="element-group-main">
+<div class="min-padding">
+<div class="layout-render no-padding flex-row flex-nowrap">
+<div class="render-item" data-draggable="true">
+<div class="element-main element-type-text-single component-render" data-v-5cee62ab="">&nbsp;</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>`,type:"text",boxItem:{x:843,y:0,h:108,w:297,active:!1},active:!1},{id:"1682933033144",content:`<div class="render-item" data-draggable="true">
+<div class="element-group-main">
+<div class="min-padding">
+<div class="layout-render cvo-text-color no-padding flex-row flex-nowrap">
+<div class="render-item" data-draggable="true">
+<div class="element-main element-type-text-single component-render" data-v-5cee62ab="">
+<div class="ql-editor" data-v-5cee62ab="">
+<p><strong class="ql-font-Barlow ql-size-18px">SKILLS </strong></p>
+<ul>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Proficient in Vue.js, HTML, CSS, and JavaScript.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Familiar with TypeScript and Vue2, Vue3.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Familiar with SOLID, DRY, and KISS principles.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Experience with frontend build tools such as Webpack and Babel.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Knowledge of CSS preprocessors such as SASS and LESS.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Familiarity with version control systems such as Git.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Strong problem-solving and analytical skills.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Experience with testing tools such as unittest and vitest.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Familiar with Node.js, Express, and MongoDB.</span></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="ddict_btn" style="top: 58px; left: 582.167px;"><img src="chrome-extension://bpggmmljdiliancllaapiggllnkbjocb/logo/48.png" /></div>`,type:"text",boxItem:{x:60,y:255,h:230,w:1080,active:!1},active:!0},{id:"1682933085815",content:`<div class="render-item" data-draggable="true">
+<div class="element-group-main">
+<div class="min-padding">
+<div class="layout-render cvo-text-color no-padding flex-row flex-nowrap">
+<div class="render-item" data-draggable="true">
+<div class="element-main element-type-text-single component-render" data-v-5cee62ab="">
+<div class="ql-editor" data-v-5cee62ab="">
+<p><strong class="ql-font-Barlow ql-size-18px">EXPERIENCE </strong></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="ddict_btn" style="top: 58px; left: 582.167px;"><img src="chrome-extension://bpggmmljdiliancllaapiggllnkbjocb/logo/48.png" /></div>
+<div class="ddict_btn" style="top: 60px; left: 582.167px;"><img src="chrome-extension://bpggmmljdiliancllaapiggllnkbjocb/logo/48.png" /></div>`,type:"text",boxItem:{x:60,y:490,h:56,w:125,active:!1},active:!1},{id:"1682933157167",content:`<div class="render-item" data-draggable="true">
+<div class="element-main element-type-text-single component-render" data-v-5cee62ab="">
+<div class="ql-editor" data-v-5cee62ab="">
+<p><strong class="ql-font-Barlow ql-size-13px">NEXTPAY</strong></p>
+</div>
+</div>
+</div>
+<div class="render-item" data-draggable="true">
+<div class="element-group-main">
+<div class="min-padding">
+<div class="layout-render no-padding flex-row flex-wrap">
+<div class="render-item" data-draggable="true">
+<div class="element-main element-type-text-single component-render" data-v-5cee62ab="">
+<div class="ql-editor" data-v-5cee62ab="">
+<p><span class="ql-font-Barlow ql-size-13px">7-2022 </span>-&gt; Present</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>`,type:"text",boxItem:{x:60,y:550,h:74,w:238,active:!1},active:!1},{id:"1682933228260",content:`<div class="render-item" data-draggable="true">
+<div class="element-group-main">
+<div class="min-padding">
+<div class="layout-render cvo-text-color no-padding flex-row flex-nowrap">
+<div class="render-item" data-draggable="true">
+<div class="element-main element-type-text-single component-render" data-v-5cee62ab="">
+<div class="ql-editor" data-v-5cee62ab="">
+<div class="render-item" data-draggable="true">
+<div class="element-main element-type-text-single component-render" data-v-5cee62ab="">
+<div class="ql-editor" data-v-5cee62ab="">
+<p><strong class="ql-font-Barlow ql-size-13px">FRONTEND DEVELOPER</strong></p>
+</div>
+</div>
+</div>
+<ul>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Developed and maintained Vue.js web applications, </span><strong class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">NEXTSHOP </strong><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">sales management software.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Implemented new features such as </span><strong class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">order return</strong><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto"> and </span><strong class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">delivery order</strong><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Improved user experience of various features including</span><strong class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto"> in-store sales, customer management, supplier management, debt management, </strong><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">and </span><strong class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">report.</strong></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Collaborated with designers to implement responsive designs and intuitive user experiences.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Optimized application performance by implementing lazy loading, code splitting, and other techniques.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Utilized Vuex for state management and Vue Router for navigation.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Worked with RESTful APIs to integrate backend functionality.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Participated in code reviews and contributed to team knowledge sharing .</span></li>
+<li>Collaborated with project managers and designers to ensure timely delivery of projects</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="ddict_btn" style="top: 58px; left: 582.167px;"><img src="chrome-extension://bpggmmljdiliancllaapiggllnkbjocb/logo/48.png" /></div>
+<div class="ddict_btn" style="top: 66px; left: 582.167px;"><img src="chrome-extension://bpggmmljdiliancllaapiggllnkbjocb/logo/48.png" /></div>`,type:"text",boxItem:{x:309,y:550,h:250,w:833,active:!1},active:!1},{id:"1682936527770",content:`<div class="render-item" data-draggable="true">
+<div class="element-main element-type-text-single component-render" data-v-5cee62ab="">
+<div class="ql-editor" data-v-5cee62ab="">
+<p><strong class="ql-font-Barlow ql-size-13px">BIGMAX </strong></p>
+</div>
+</div>
+</div>
+<div class="render-item" data-draggable="true">
+<div class="element-group-main">
+<div class="min-padding">
+<div class="layout-render no-padding flex-row flex-wrap">
+<div class="render-item" data-draggable="true">
+<div class="element-main element-type-text-single component-render" data-v-5cee62ab="">
+<div class="ql-editor" data-v-5cee62ab="">
+<p><span class="ql-font-Barlow ql-size-13px">6-2020 </span>-&gt; 3-2012</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>`,type:"text",boxItem:{x:60,y:824,h:74,w:238,active:!1},active:!1},{id:"1682936608972",content:`<div class="render-item" data-draggable="true">
+<div class="element-group-main">
+<div class="min-padding">
+<div class="layout-render cvo-text-color no-padding flex-row flex-nowrap">
+<div class="render-item" data-draggable="true">
+<div class="element-main element-type-text-single component-render" data-v-5cee62ab="">
+<div class="ql-editor" data-v-5cee62ab="">
+<div class="render-item" data-draggable="true">
+<div class="element-main element-type-text-single component-render" data-v-5cee62ab="">
+<div class="ql-editor" data-v-5cee62ab="">
+<div class="render-item" data-draggable="true">
+<div class="element-main element-type-text-single component-render" data-v-5cee62ab="">
+<div class="ql-editor" data-v-5cee62ab="">
+<p><strong class="ql-font-Barlow ql-size-13px">FULL-STACK DEVELOPER</strong></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<ul>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Developed and maintained multiple Nuxt.js web applications, website</span><strong class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto"> e-commerce</strong><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto"> and website </span><strong class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">recruitment</strong><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto"> Developed and maintained full-stack web applications using Vue.js, Node.js, Express, and MongoDB.</span></li>
+<li><span class="ql-size-13px ql-lineHeight-1.8 ql-font-Roboto">Worked with both relational and non-relational databases to design efficient and scalable database schemas.</span></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="ddict_btn" style="top: 58px; left: 582.167px;"><img src="chrome-extension://bpggmmljdiliancllaapiggllnkbjocb/logo/48.png" /></div>
+<div class="ddict_btn" style="top: 66px; left: 582.167px;"><img src="chrome-extension://bpggmmljdiliancllaapiggllnkbjocb/logo/48.png" /></div>
+<div class="ddict_btn" style="top: 65px; left: 565.654px;"><img src="chrome-extension://bpggmmljdiliancllaapiggllnkbjocb/logo/48.png" /></div>`,type:"text",boxItem:{x:309,y:824,h:137,w:833,active:!1},active:!1}],container:{height:1e3}},f=i("global",()=>{const n=l({lineW:{show:!1,position:0},lineH:{show:!1,position:0},listPosition:{h:[],w:[],centerH:[600],centerW:[]},listItem:[],container:{height:1e3}}),a=localStorage.getItem("web-data");let e;return a?e=JSON.parse(a):e=g,n.listItem=e.listItem,n.container={...e.container},{data:n,getStyleContainer:()=>({height:n.container.height+"px"}),saveData:()=>{s({message:"Save data successfully!",type:"success"})}}});const v=["innerHTML"],m={__name:"ShowDragItem",props:{item:Object,isDev:{type:Boolean,default:!1}},setup(n){const a=e=>({top:e.boxItem.y+"px",left:e.boxItem.x+"px",width:e.boxItem.w+"px",height:e.boxItem.h+"px"});return(e,t)=>(o(),d("div",{class:r(["block_item",{border:n.isDev}]),innerHTML:n.item.content,style:c(a(n.item))},null,14,v))}},u=p(m,[["__scopeId","data-v-3d943a5e"]]);export{u as S,f as u};
